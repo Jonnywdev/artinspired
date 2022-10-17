59cc4374d927af5f767268b36cec73a0d6ae5e6d
Jonnywdev <jonnywhitt19@gmail.com>
2022-10-17 17:55:58 +0000

    Take out login - create new chatroom function

--- feed/urls.py
+++ feed/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
+    # path('', views.PostList.as_view(), name='home'),
+    path('', views.home, name="home"),
+    path('chatroom/', views.chatroom, name="chatroom")
 ]
--- feed/views.py
+++ feed/views.py
@@ -3,9 +3,25 @@ from django.views import generic, View
 from .models import Post
 
 
-class PostList(generic.ListView):
+# class PostList(generic.ListView):
 
-    model = Post
-    queryset = Post.objects.order_by('-created_on')
-    template_name = 'index.html'
-    paginate_by = 9
+#     model = Post
+#     queryset = Post.objects.order_by('-created_on')
+#     template_name = 'index.html'
+#     paginate_by = 9
+
+
+chatrooms = [
+    {'id': 1, 'name': 'How to make your first logo.'},
+    {'id': 2, 'name': 'What does it take to be a good designer?'},
+    {'id': 3, 'name': 'Whats the best prototype design app?'},
+]
+
+
+def chatroom(request):
+    context = {'chatrooms': chatrooms}
+    return render(request, 'chatroom.html')
+
+
+def home(request):
+    return render(request, 'index.html', {'chatrooms': chatrooms})
